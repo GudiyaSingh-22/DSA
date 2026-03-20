af19d2a542979f886ac6df5ba64a35ac64d2b135
gudiyasingh9140-lang <gudiyasingh9140@gmail.com>
2026-03-20 17:55:13 +0530

    ok

---
+++ Student.java
@@ -0,0 +1,34 @@
+public class Student {
+    int rollno;
+    String branch ;
+    String name;
+    Student(int rollno, String branche, String name){
+        rollno = rollno;
+        branch = branche;
+
+        name = name ;
+    }
+    Student(){
+        System.out.println("hello i am constrcter"); 
+    }
+    String getName(){
+        return name ;
+    }
+    void setName(String s){
+        name = s ;
+
+    }
+
+    public static void main(String[] args) {
+        Student s1 = new Student(); 
+        String name = s1.name; 
+        System.out.println(name); 
+        Student s2 = new Student(101,"cse", "govind") ;
+        Student s3= new Student(101,"cse", "gudiya") ; 
+        System.out.println(s2.getName()) ;
+        s3.setName("Gudiya Singh");
+        System.out.println(s3.getName()); 
+        System.out.println(s1.name);
+       }
+}
+ 
