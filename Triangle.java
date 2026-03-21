8933b974f1e95494d8ee4f12be8b77f657f6212e
gudiyasingh9140-lang <gudiyasingh9140@gmail.com>
2026-03-21 21:00:07 +0530

    area

--- Triangle.java
+++ Triangle.java
@@ -4,5 +4,23 @@ public class Triangle {
     Triangle(){
         System.out.println("Area of triangle:");
     }
-    
+    Triangle(int b,int h){
+        base = b;
+        height = h;
+    }
+   /* void area(){
+        System.out.println((base*height)/2);
+    }
+    public static void main(String[] args) {
+        Triangle t1 = new Triangle (45,40);
+        t1.area();
+    }*/
+   int getArea(){
+ return (base*height/2);
+   }
+   public static void main(String[] args) {
+    Triangle t1 = new Triangle(4,5);
+    //System.out.println(t1.getArea());
+    t1.getArea();
+   }
 }
