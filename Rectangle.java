6a96659cbc3eb99265b7da1c0b8e8175b37e38d5
gudiyasingh9140-lang <gudiyasingh9140@gmail.com>
2026-03-21 20:33:06 +0530

    function

---
+++ Rectangle.java
@@ -0,0 +1,24 @@
+public class Rectangle {
+    int length;
+    int width;
+    Rectangle(){
+        System.out.println("Area and perimeter:");
+    }
+    Rectangle(int l,int w){
+        length = l;
+        width = w;
+    }
+    void area(){
+        System.out.println(length*width);
+    }
+    void perimeter(){
+        System.out.println(2*(length+width));
+    }
+    public static void main(String[] args) {
+        Rectangle v1 = new Rectangle(5,6);
+        v1.area();
+        Rectangle v2 = new Rectangle(20,15);
+        v2.perimeter();
+    }
+    }
+
