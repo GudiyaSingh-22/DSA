ebc3bdd00b7df6f88bc5480b709fe4dbd9bd097b
gudiyasingh9140-lang <gudiyasingh9140@gmail.com>
2026-06-20 22:15:47 +0530

    string basic

---
+++ JavaProgram/StringProgram/Methods.java
@@ -0,0 +1,10 @@
+package JavaProgram.StringProgram;
+
+import java.util.Arrays;
+
+public class Methods {
+    public static void main(String[] args) {
+        String name = "Gudiya Singh";
+        System.out.println(Arrays.toString(name.toCharArray()));
+    }
+}
--- JavaProgram/StringProgram/Performance.java
+++ JavaProgram/StringProgram/Performance.java
@@ -2,12 +2,22 @@ package JavaProgram.StringProgram;
 
 public class Performance {
     public static void main(String[] args) {
-        String series = "";
-        for (int i = 0; i < 26; i++) {
-            char ch = (char)('a'+1);
-           // series = series + ch;
+     //   String series = "";
+    //     for (int i = 0; i < 26; i++) {
+    //         char ch = (char)('a'+ i);
+    //        //System.out.println(ch);
+    //        series = series + ch;
+    //     }
+    //    System.out.println(series);
+
+
+    //performing the same problem using string builder 
+    StringBuilder builder = new StringBuilder();
+    for (int i = 0; i < 26; i++) {
+        char ch = (char)('a'+ i);
+        builder.append(ch);
         }
-       // System.out.println(series);
+        System.out.println(builder.toString());
     }
     
 }
