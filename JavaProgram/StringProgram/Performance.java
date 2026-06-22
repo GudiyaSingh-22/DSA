57f42f3fdd015152090072226ee91ddea538160d
gudiyasingh9140-lang <gudiyasingh9140@gmail.com>
2026-06-22 22:40:50 +0530

    .

--- JavaProgram/StringProgram/Performance.java
+++ JavaProgram/StringProgram/Performance.java
@@ -12,11 +12,11 @@ public class Performance {
 
 
     //performing the same problem using string builder 
-    StringBuilder builder = new StringBuilder();
-    for (int i = 0; i < 26; i++) {
-        char ch = (char)('a'+ i);
-        builder.append(ch);
-        }
-        System.out.println(builder.toString());
-    }
-}
+//     StringBuilder builder = new StringBuilder();
+//     for (int i = 0; i < 26; i++) {
+//         char ch = (char)('a'+ i);
+//         builder.append(ch);
+//         }
+//         System.out.println(builder.toString());
+     }
+ }
