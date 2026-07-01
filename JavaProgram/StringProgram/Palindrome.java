40b19f6c6d159f412fd52a30133100079fb1da54
gudiyasingh9140-lang <gudiyasingh9140@gmail.com>
2026-07-01 23:39:26 +0530

    exploring patterns

--- JavaProgram/StringProgram/Palindrome.java
+++ JavaProgram/StringProgram/Palindrome.java
@@ -2,7 +2,22 @@ package JavaProgram.StringProgram;
 
 public class Palindrome {
     public static void main(String[] args) {
-        
+        String str = "Gudiya";
+        System.out.println(isPalimdrome(str)); 
+    }
+    static boolean isPalimdrome(String str){
+        if (str == null || str.length() == 0) {
+            return true;
+        }
+        str = str.toLowerCase();
+        for (int i = 0; i < str.length(); i++) {
+            char start = str.charAt(i);
+            char end = str.charAt(str.length()-i-1);
+            if (start!=end) {
+                return false;
+            }
+        }
+        return true;
     }
     
 }
--- JavaProgram/StringProgram/ProA.java
+++ JavaProgram/StringProgram/ProA.java
@@ -1,5 +1,15 @@
 package JavaProgram.StringProgram;
 
 public class ProA {
-    
+ public static void main(String[] args) {
+    pattern2(5);
+ }  
+ static void pattern2(int n){
+    for (int i = 1; i <=n; i++) {
+        for (int j = 1; j<=n; j++) {
+            System.out.print("*");
+        }
+        System.out.println();
+    }
+ } 
 }
