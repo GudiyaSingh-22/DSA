460f1a673ae7b20d662629bd9d626f87575c3b5c
gudiyasingh9140-lang <gudiyasingh9140@gmail.com>
2026-07-22 23:51:10 +0530

    Problem solving

---
+++ JavaProgram/Searching/Leetcode34.java
@@ -0,0 +1,39 @@
+package JavaProgram.Searching;
+
+public class Leetcode34 {
+    public static void main(String[] args) {
+        int[]nums={5,7,7,8,8,10};
+        int target = 8;
+        int ans = searchRange(int[]nums,target);
+        System.out.println(ans);
+    }
+    public int[] searchRange(int[] nums, int target) {
+        int[] ans = {-1,-1};
+        int start = search(nums, target, true);
+        int end = search(nums, target, false);
+        ans[0] = start;
+        ans[1] = end;
+        return ans;
+    }
+    int search(int[]nums,int target,boolean findStartIndex){
+        int ans=-1;
+        int start = 0;
+        int end = nums.length-1;
+        while (start<=end) {
+            int mid = start+(end-start)/2;
+            if (target<nums[mid]) {
+                end=mid-1;
+            }
+            else if (target==nums[mid]) {
+                ans= mid;
+                if(findStartIndex){
+                  end = mid -1;  
+                }
+            }
+            else{
+                start=mid+1;
+            }
+        }
+    return ans;
+    }
+    }
