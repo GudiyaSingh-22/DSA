4426d4965d1fd4481d34594eb835361028aac7d6
gudiyasingh9140-lang <gudiyasingh9140@gmail.com>
2026-07-28 22:55:21 +0530

    Brute force problem

--- gudiya/Main.java
+++ gudiya/Main.java
@@ -1,5 +1,26 @@
-package gudiya;
+import java.util.Scanner; 
 
 public class Main {
-    
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        int x = sc.nextInt();
+        int y = sc.nextInt();
+
+        int ans;
+
+        if (x < y) {
+            ans = y - x;
+        } else {
+            int diff = x - y;
+
+            if (diff % 2 == 0) {
+                ans = diff / 2;
+            } else {
+                ans = 1 + (diff + 1) / 2;
+            }
+        }
+
+        System.out.println(ans);
+    }
 }
