ef8ae3aaed8300f974973cbfafaf425914eab87f
gudiyasingh9140-lang <gudiyasingh9140@gmail.com>
2026-08-01 20:09:50 +0530

    Learning Recursive Binary Search

--- JavaProgram/Recursion/Example.java
+++ JavaProgram/Recursion/Example.java
@@ -1,5 +1,13 @@
 package JavaProgram.Recursion;
 
 public class Example {
-    
+   public static void main(String[] args) {
+    System.out.println(fibo(6));
+   }
+   static int fibo(int n){
+    if(n<2){
+        return n ;
+    }
+    return (fibo(n-2)+fibo(n-1));
+   } 
 }
---
+++ JavaProgram/Recursion/RecursiveBS.java
@@ -0,0 +1,22 @@
+package JavaProgram.Recursion;
+
+public class RecursiveBS {
+    public static void main(String[] args) {
+        int [] arr = {2,4,6,8,10,12,14};
+        int target = 8;
+        System.out.println(search(arr, target, 0, arr.length-1));
+    }
+    static int search(int[]arr, int target , int start , int end){   
+        if (start>end) {
+            return -1;
+        }
+         int mid = start + (end -start)/2;
+        if (target==arr[mid]) {
+            return mid;
+        }
+        if (target<arr[mid]) {
+            return search(arr, target, start, mid-1);
+        }
+        return search(arr, target, mid+1, end);
+    }
+}
