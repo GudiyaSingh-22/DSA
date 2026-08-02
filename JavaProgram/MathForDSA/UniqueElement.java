4f74a37235575371867d61780a97936df26bdb74
gudiyasingh9140-lang <gudiyasingh9140@gmail.com>
2026-08-02 15:06:37 +0530

    Finding unique element in a array

---
+++ JavaProgram/MathForDSA/UniqueElement.java
@@ -0,0 +1,15 @@
+package JavaProgram.MathForDSA;
+
+public class UniqueElement {
+    public static void main(String[] args) {
+        int [] arr = {2,3,4,1,2,1,3,6,4};
+        System.out.println(Unique(arr));
+    }
+    static int Unique(int[]arr){
+        int unique = 0;
+        for (int i : arr) {
+             unique ^= i;
+        }
+        return unique;
+    }
+}
