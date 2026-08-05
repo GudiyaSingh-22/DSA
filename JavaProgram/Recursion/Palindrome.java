e2ac2e75602ff18d99c98555380977faad428410
gudiyasingh9140-lang <gudiyasingh9140@gmail.com>
2026-08-05 14:41:09 +0530

    Checking wheather it is palindrome or not

--- JavaProgram/Recursion/Palindrome.java
+++ JavaProgram/Recursion/Palindrome.java
@@ -1,11 +1,10 @@
 package JavaProgram.Recursion;
 
-public class RevDigits {
-    static int rev2(int n) {
+public class Palindrome {
+    static int rev(int n) {
     int digits = (int)(Math.log10(n)) + 1;
     return helper(n, digits);
 }
-
 private static int helper(int n, int digits) {
     if (n % 10 == n) {
         return n;
@@ -14,7 +13,10 @@ private static int helper(int n, int digits) {
     return rem * (int)(Math.pow(10, digits - 1))
             + helper(n / 10, digits - 1);
 }
+static boolean palin(int n){
+    return n == rev(n);
+}
 public static void main(String[] args) {
-    System.out.println(rev2(1234));
+    System.out.println(palin(1234321));
 }
 }
